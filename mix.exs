ef1bbfca3479734a6e0ee97e426bab07457ad535
sawater <d.chaser.zsy@gmail.com>
2017-11-11 00:24:21 +0800

    up

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Vayne.Mixfile do
     [
       app: :vayne,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
